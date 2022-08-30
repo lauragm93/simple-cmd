771e1343c2bd130e9bba629fd8de8379d9bdba74
marius <marius.mueller@capgemini.com>
2022-08-30 11:39:49 +0200

    feat: MkdirCommand
    Command zum erstellen von Directories oder Files

---
+++ src/main/java/cmd/commands/mkdir/MkdirCommand.java
@@ -0,0 +1,42 @@
+package cmd.commands.mkdir;
+
+import cmd.SimpleCmd;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@CommandLine.Command(
+        name = "mkdir",
+        description = "Creates directories or files",
+        mixinStandardHelpOptions = true)
+public class MkdirCommand implements Runnable{
+
+    private static final Logger LOG = LoggerFactory.getLogger(MkdirCommand.class);
+    @CommandLine.Parameters(index = "0", description = "path of file/directory to create")
+    private String pathToFileOrDirectory;
+    @CommandLine.Option(names = {"-f", "--file"}, description = "create file")
+    private boolean isFile;
+
+
+    @Override
+    @SuppressWarnings("java:S112")
+    public void run() {
+        Path toCreate = SimpleCmd.getCurrentLocation().toPath().resolve(Paths.get(pathToFileOrDirectory));
+        try {
+                if (!isFile) {
+                    Files.createDirectories(toCreate);
+                }else{
+                  Files.createFile(toCreate);
+                }
+                LOG.debug("{} wurde erstellt.",toCreate);
+            } catch (IOException e) {
+                LOG.error("{} konnte nicht erstellt werden",toCreate);
+                throw new RuntimeException(e);
+            }
+    }
+
+}
