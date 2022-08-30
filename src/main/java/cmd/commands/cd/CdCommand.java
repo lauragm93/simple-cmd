3e4d45292cdcaed61f01532182a0ffd80a47a516
Aykutalp Coskun <aykutalp.coskun@capgemini.com>
2022-08-30 17:19:19 +0200

    Implemented CdCommand

---
+++ src/main/java/cmd/commands/cd/CdCommand.java
@@ -0,0 +1,38 @@
+package cmd.commands.cd;
+
+import cmd.SimpleCmd;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * "Change Directory" command class
+ * <p/>
+ * Executing the command changes the current directory.
+ * Optionally the.
+ */
+@Command(
+        name = "cd",
+        description = "Changes the directory",
+        mixinStandardHelpOptions = true)
+public class CdCommand implements Runnable {
+
+    @Parameters(index = "0", description = "absolute path of the directory to change to")
+    private File path;
+
+    /*public CdCommand() {
+
+    }*/
+
+    @Override
+    public void run() {
+        if(path.exists() && path.isDirectory() && path.isAbsolute())
+            SimpleCmd.setCurrentLocation(path);
+        else if(path.exists() && path.isDirectory())
+            SimpleCmd.setCurrentLocation(SimpleCmd.getCurrentLocation().toPath().resolve(path.toPath()).toFile());
+    }
+}
